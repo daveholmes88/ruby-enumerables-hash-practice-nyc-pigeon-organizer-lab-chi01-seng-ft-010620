0088e74b3605a7f3662b14f818242a3f411f5eb5
David Holmes <phoenix1988@gmail.com>
2020-01-03 01:47:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  array = []
+  data.each do |memo, pair|
+    pair.each do |color, result|
+      array << result
+    end
+  end
+  array.flatten!
+  array.uniq!
+  i = 0
+  while i < array.length
+    hash[array[i]] = {
+      :color => [],
+      :gender => [],
+      :lives => []
+    }
+    i += 1
+  end
+  hash
 end
